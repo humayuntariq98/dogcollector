826310cf57d239b126fe7eeda09afef8be39730a
humayuntariq98 <hammytar23@gmail.com>
2023-09-21 01:19:12 +0500

    dog index page template, view and url

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+dogs = [
+  {'name': 'Cleo', 'breed': 'German Shephard', 'description': 'great for protection', 'age': 3},
+  {'name': 'Clover', 'breed': 'Husky', 'description': 'gentle and loving', 'age': 2},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def dogs_index(request):
+    return render(request, 'dogs/index.html', {
+      'dogs': dogs  
+    })
