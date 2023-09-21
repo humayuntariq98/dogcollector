d2cd720773e46fcd5f228d14541bac296127da85
humayuntariq98 <hammytar23@gmail.com>
2023-09-21 22:19:09 +0500

    refactor home page template, create dog class model

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 
-dogs = [
-  {'name': 'Cleo', 'breed': 'German Shephard', 'description': 'great for protection', 'age': 3},
-  {'name': 'Clover', 'breed': 'Husky', 'description': 'gentle and loving', 'age': 2},
-]
-
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
